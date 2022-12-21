4ce82bac35db92b40686498318380939bc8b5b05
g-otn <g-otn@outlook.com>
2022-12-21 02:12:49 -0300

    Initial commit (P11 files)

---
+++ README.md
@@ -0,0 +1,4 @@
+# godot-action-rpg-course
+Simple action RPG game made in Godot following HeartBeast's "Godot Action RPG" course.
+
+Assets extracted from [uheartbeast/youtube-tutorials](https://github.com/uheartbeast/youtube-tutorials/tree/master/Action%20RPG).
---
+++ World/Grass.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+func create_grass_effect():
+	var GrassEffect = load("res://Effects/GrassEffect.tscn")
+	var grassEffect = GrassEffect.instance()
+	var world = get_tree().current_scene
+	world.add_child(grassEffect)
+	grassEffect.global_position = global_position
+
+func _on_Hurtbox_area_entered(area):
+	create_grass_effect()
+	queue_free()
