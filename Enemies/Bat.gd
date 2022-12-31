29549ece7ddaffa5fdea6ee93c00d94d78866f2c
g-otn <g-otn@outlook.com>
2022-12-31 18:00:20 -0300

    update: files to P21

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -5,6 +5,7 @@ export(int) var MAX_SPEED = 40
 export(int) var FRICTION = 90
 export(int) var KNOCKBACK_SPEED = 100
 export(int) var SOFT_COLLISION_PUSH = 150
+export(int) var WANDER_TARGET_RANGE = 500
 
 const EnemyDeathEffect = preload("res://Effects/EnemyDeathEffect.tscn")
 
@@ -24,6 +25,7 @@ onready var stats = $Stats
 onready var playerDetectionZone = $PlayerDetectionZone
 onready var hurtbox = $Hurtbox
 onready var softCollision = $SoftCollision
+onready var wanderController = $WanderController
 
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, FRICTION * delta)
@@ -33,27 +35,47 @@ func _physics_process(delta):
 		IDLE:
 			velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 			seek_player()
+			
+			if wanderController.get_time_left() == 0:
+				pick_state_and_wander()
 		
 		WANDER:
-			pass
-		
+			seek_player()
+			if wanderController.get_time_left() == 0:
+				pick_state_and_wander()
+			
+			accelerate_towards_point(wanderController.target_position, delta)
+			
+			if global_position.distance_to(wanderController.target_position) <= WANDER_TARGET_RANGE:
+				pick_state_and_wander()
 		CHASE:
 			var player = playerDetectionZone.player
 			if player != null:
-				var direction = (player.global_position - global_position).normalized()
-				velocity = velocity.move_toward(direction * MAX_SPEED, ACCELLERATION * delta)
+				accelerate_towards_point(player.global_position, delta)
 			else:
 				state = IDLE
-			sprite.flip_h = velocity.x < 0
 	
 	if softCollision.is_colliding():
 		velocity += softCollision.get_push_vector() * delta * SOFT_COLLISION_PUSH
 	velocity = move_and_slide(velocity)
 
+func pick_state_and_wander():
+	state = pick_random_state([IDLE, WANDER])
+	wanderController.start_wander_timer(rand_range(1, 3))
+
+func accelerate_towards_point(point, delta):
+	var direction = global_position.direction_to(wanderController.target_position)
+	velocity = velocity.move_toward(direction * MAX_SPEED, ACCELLERATION * delta)
+	sprite.flip_h = velocity.x < 0
+
 func seek_player():
 	if playerDetectionZone.can_see_player():
 		state = CHASE
 
+func pick_random_state(state_list):
+	state_list.shuffle()
+	return state_list.pop_front()
+	
 func _on_Hurtbox_area_entered(area):
 	stats.health -= area.damage
 	# Ignores bat chase vector
